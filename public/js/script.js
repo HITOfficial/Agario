4fa32d47a9f1e0eb4ca52a06c8b430b7658ad8ff
Tomasz Bochnak <tomaszbochnak00@gmail.com>
2020-07-24 08:55:37 +0200

    Blob points generate functions

--- public/js/script.js
+++ public/js/script.js
@@ -1,22 +1,51 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
+let newBlobPoint = {
+    id : 0,
+    x : null,
+    y : null,
+    radius : null
+}
 let playerBlob = {
-    x : canvas.width / 2,
-    y : canvas.height / 2,
-    radius : 20
+    id : 1,
+    position : {
+        x : canvas.width / 2,
+        y : canvas.height / 2,
+    },
+    range : {
+        radius : 20
+    }
 };
 let mouseCoord = {
     x : null,
     y : null,
 }
-blobs = [];
+blobPointArray = [];
 const createBlob = (x, y, radius) => {
     ctx.beginPath();
     ctx.arc(x, y, radius, 0, 2 * Math.PI);
     ctx.fill();
 }
-createBlob(playerBlob.x, playerBlob.y, playerBlob.radius);
+const generateBlobPoint = () => {
+    radius = 5;
+    x = Math.floor(Math.random() * (canvas.width - (radius * 2)) + radius);
+    y = Math.floor(Math.random() * (canvas.height - (radius * 2)) + radius);
+    newBlobPoint.id += 1;
+    newBlobPoint.x = x;
+    newBlobPoint.y = y;
+    newBlobPoint.radius = radius;
+}
+createBlobPoint = () => {
+    for(i = 0; i < 20; i++){
+        generateBlobPoint();
+        createBlob(newBlobPoint.x, newBlobPoint.y, newBlobPoint.radius);
+        blobPointArray.push(newBlobPoint);    
+    }
+}
+createBlob(playerBlob.position.x, playerBlob.position.y, playerBlob.range.radius);
 canvas.addEventListener('mousemove', (mouse) => {
     mouseCoord.x = mouse.screenX;
     mouseCoord.y = mouse.screenY;
+    createBlob(mouseCoord.x + (playerBlob.range.radius / 2), mouseCoord.y - (playerBlob.range.radius * 3), playerBlob.range.radius);
+    playerBlob.position = mouseCoord;
 });
